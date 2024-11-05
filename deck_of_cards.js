25ad62cfef5bf1c2a36b148d964c90671cfabe37
Luis Irizarry <lirizarry6178@gmail.com>
2024-11-05 01:01:23 -0500

    end of exercise

--- deck_of_cards.js
+++ deck_of_cards.js
@@ -1,42 +1,60 @@
-$(document).ready(function() {
-    $buttonGimme = $("#gimme");
-    $buttonRest = $("#reset");
-    $img = $("img");
+$(document).ready(async function() {
+    const $buttonGimme = $("#gimme");
+    const $buttonRest = $("#reset");
+    const $img = $("img");
 
-    let deck_id;
-    newDeck()
-        .then((id) => {
-            deck_id = id;
-        })
-        .catch((err) => console.log(err));
+    // Disable reset button initially
+    $buttonRest.attr("disabled", true);
 
-    $buttonGimme.on("click", function() {
-        axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`)
-            .then((card) => {
-                $img.attr("src", card.data.cards[0].image);
-                if (card.data.remaining === 1) {
-                    $buttonGimme.attr("disabled", true);
-                    $buttonRest.attr("disabled", false);
-                }
-            })
-            .catch((err) => console.log(err));
+    // Set the initial deck ID
+    let deck_id = await newDeck();
+
+    // Set up click event handlers using async functions
+    $buttonGimme.on("click", async function() {
+        await gimme(deck_id);
     });
 
-    $buttonRest.on("click", function() {
-        newDeck()
-            .then((id) => {
-                deck_id = id; // Get the new deck ID
-                $img.attr("src", "");
-                $buttonGimme.attr("disabled", false);
-                $buttonRest.attr("disabled", true);
-            })
-            .catch((err) => console.log(err));
+    $buttonRest.on("click", async function() {
+        deck_id = await reset();
     });
 });
 
 
-function newDeck() {
-    return axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-        .then((deck) => deck.data.deck_id)
-        .catch((err) => console.error(err));
+async function newDeck() {
+    try {
+        let res = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+        return res.data.deck_id;
+    } catch (e) {
+        console.log("Error", e);
+    }
+}
+
+async function gimme(deck_id) {
+    try {
+        let card = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`);
+        $("img").attr("src", card.data.cards[0].image);
+        
+        // Disable the "Gimme" button and enable the "Reset" button if only 1 card is remaining
+        if (card.data.remaining === 1) {
+            $("#gimme").attr("disabled", true);
+            $("#reset").attr("disabled", false);
+        }
+    } catch (e) {
+        console.log("Error", e);
+    }
+}
+
+async function reset() {
+    try {
+        // Clear the image and reset button states
+        $("img").attr("src", "");
+        $("#gimme").attr("disabled", false);
+        $("#reset").attr("disabled", true);
+        
+        // Fetch a new deck ID for the reset
+        let newDeckId = await newDeck();
+        return newDeckId;
+    } catch (e) {
+        console.log("Error", e);
+    }
 }
--- number_facts.js
+++ number_facts.js
@@ -1,34 +1,38 @@
-$h1 = $("h1");
+// Setting the text for the h1 element
+const $h1 = $("h1");
 $h1.text("Number Facts");
 
-$ul = $("ul");
+const $ul = $("ul");
 
-// Single favorite number fact request
-axios.get('http://numbersapi.com/7?json')
-  .then((fact) => {
-    $ul.append(`<li>Single favorite number fact: ${fact.data.text}</li>`);
-  })
-  .catch((err) => console.log(err));
-
-// Multiple number facts in a single request
-axios.get('http://numbersapi.com/1..4?json')
-  .then((facts) => {
-    for (let key in facts.data) {
-      $ul.append(`<li>Fact for number ${key}: ${facts.data[key]}</li>`);
-    }
-  })
-  .catch((err) => console.log(err));
-
-// Four facts about the same favorite number
-const favoriteNumberFacts = [];
-for (let i = 0; i < 4; i++) {
-  favoriteNumberFacts.push(axios.get('http://numbersapi.com/7?json'));
+// Function to get a single favorite number fact
+async function getSingleFavoriteFact() {
+  let factData = await axios.get('http://numbersapi.com/7?json');
+  $ul.append(`<li>Single favorite number fact: ${factData.data.text}</li>`);
 }
 
-Promise.all(favoriteNumberFacts)
-  .then((factsArr) => {
-    for (let fact of factsArr) {
-      $ul.append(`<li>Favorite number fact: ${fact.data.text}</li>`);
-    }
-  })
-  .catch((err) => console.log(err));
+// Function to get multiple facts in a single request
+async function getMultipleFacts() {
+  let factsData = await axios.get('http://numbersapi.com/1..4?json');
+  for (let key in factsData.data) {
+    $ul.append(`<li>Fact for number ${key}: ${factsData.data[key]}</li>`);
+  }
+}
+
+// Function to get multiple facts for the same number
+async function favoriteNumberFacts() {
+  let facts = await Promise.all([
+    axios.get('http://numbersapi.com/7?json'),
+    axios.get('http://numbersapi.com/7?json'),
+    axios.get('http://numbersapi.com/7?json'),
+    axios.get('http://numbersapi.com/7?json')
+  ]);
+
+  facts.forEach(fact => {
+    $ul.append(`<li>Fact for number 7: ${fact.data.text}</li>`);
+  });
+}
+
+// Call functions to load facts
+getSingleFavoriteFact();
+getMultipleFacts();
+favoriteNumberFacts();
